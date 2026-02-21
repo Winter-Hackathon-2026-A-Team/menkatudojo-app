05dd0d1b4b42ea32af553e7170e66dfa56523104
nao-nba <ba.d.yoa.b.tw@gmail.com>
2026-02-21 09:58:49 +0900

    mainlayoutの調整

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -3,10 +3,24 @@ import { GlobalMessageBar } from '@/components/layout/GlobalMessageBar';
 import { Box, Container } from '@mui/material';
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => (
-  <Box>
+  <Box
+    sx={{
+      bgcolor: 'background.default',
+      minHeight: '100vh',
+      width: '100%',
+    }}
+  >
     <Header />
     <GlobalMessageBar />
-    <Container maxWidth="lg" sx={{ maxWidth: { lg: '1000px' }, mt: 4, px: { xs: 2, lg: 0 } }}>
+    <Container
+      maxWidth="lg"
+      sx={{
+        maxWidth: { lg: '1000px' },
+        mt: 4,
+        px: { xs: 2, lg: 0 },
+        pb: 4,
+      }}
+    >
       {children}
     </Container>
   </Box>
